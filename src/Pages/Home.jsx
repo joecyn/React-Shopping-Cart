2241709f96a893e8952df2d1f7659f9e154cea17
Johana Gitari <johanagitari@gmail.com>
2022-11-24 01:08:38 +0300

    Loading functionality added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,13 +8,16 @@ import { Cartcontext } from "../context/cartContext";
 
 const Homepage = () => {
   const [products, setProducts] = useState([]);
+  const[isLoading,setisLoading]=useState(false)
   const fetchData = async () => {
     const response = await axios.get("https://fakestoreapi.com/products");
     setProducts(response.data);
     //console.log(data);
   };
   useEffect(() => {
+    
     fetchData();
+    setisLoading(true)
   }, []);
   const Globalstate = useContext(Cartcontext);
   const dispatch = Globalstate.dispatch;
@@ -23,27 +26,39 @@ const Homepage = () => {
     
     <Card  >
       <div className="Prod">
-      {products.map((product, index) => {
-        product.quantity = 1;
-        return (
-          <Card.Body className="card" key={index}>
-           <Card.Img variant="top" src={product.image} />
-            
-            <Card.Text>
-            {product.title.length > 20 ? (
-              <p>{product.title.slice(0,10)}...</p>
-            ):(
-              <p>{product.title}</p>
-            )}
-            </Card.Text>
-            <h6>$. {product.price}</h6>
-            <Button onClick={() => dispatch({ type: "ADD", payload: product })}>
-              add to cart
-            </Button>
+        {isLoading ?(
           
-          </Card.Body>
-        );
-      })}
+          <ul class="fa-ul">
+               <li><i class="fa-li fa fa-spinner fa-spin fa-5x"></i></li>
+          </ul>
+
+
+        ):(
+          <>{products.map((product, index) => {
+            product.quantity = 1;
+            return (
+              <Card.Body className="card" key={index}>
+               <Card.Img variant="top" src={product.image} />
+                
+                <Card.Text>
+                {product.title.length > 20 ? (
+                  <p>{product.title.slice(0,10)}...</p>
+                ):(
+                  <p>{product.title}</p>
+                )}
+                </Card.Text>
+                <h6>$. {product.price}</h6>
+                <Button onClick={() => dispatch({ type: "ADD", payload: product })}>
+                  add to cart
+                </Button>
+              
+              </Card.Body>
+            );
+          })}
+          
+        </>
+        )}
+        
       </div>
     </Card>
     
